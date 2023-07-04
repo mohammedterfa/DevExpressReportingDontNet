64756af1b422ecb9a5adb9f006568a5a1cf52c02
mohammedterfa <mohammedterfa@gmail.com>
2023-07-04 19:33:27 +0300

    config , installed devexpress

--- DevExpressTest/Program.cs
+++ DevExpressTest/Program.cs
@@ -1,7 +1,19 @@
+using DevExpress.AspNetCore;
+using DevExpress.AspNetCore.Reporting;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Config for DevExpress
+builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+builder.Services.AddDevExpressControls();
+
+builder.Services.ConfigureReportingServices(configurator =>
+{
+    configurator.DisableCheckForCustomControllers();
+});
 
 var app = builder.Build();
 
@@ -16,9 +28,17 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "node_modules")),
+    RequestPath = "/node_modules"
+});
+
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseDevExpressControls();
 
 app.MapControllerRoute(
     name: "default",
